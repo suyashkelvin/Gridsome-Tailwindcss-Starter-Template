cb6be53c17cc8f1564d643d28e6ea0bac821614b
Suyash Kelvin Savant <62336754+suyashkelvin@users.noreply.github.com>
2021-05-24 18:16:47 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+require('~/main.css')
+
+import DefaultLayout from '~/layouts/Default.vue'
+
+export default function (Vue, { router, head, isClient }) {
+  // Set default layout as a global component
+    Vue.component('Layout', DefaultLayout)
+
+}
